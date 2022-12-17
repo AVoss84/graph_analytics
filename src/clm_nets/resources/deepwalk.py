fa3c4ef5bd8fdd627239382237c2ef88b937d2ae
Alexander Vosseler <a.vosseler@gmx.net>
2022-12-17 15:45:52 +0100

    yoochoose analysis nb

--- src/clm_nets/resources/deepwalk.py
+++ src/clm_nets/resources/deepwalk.py
@@ -22,7 +22,7 @@ class DeepWalk:
 
     def random_walk(self, g: nx.Graph, start: str, use_probabilities: bool = False) -> List[str]:
         """
-        Generate a random walk starting on start
+        Generate a random walk trajectory starting on start
         :param g: Graph
         :param start: starting node for the random walk
         :param use_probabilities: if True take into account the weights assigned to each edge to select the next candidate
@@ -50,10 +50,10 @@ class DeepWalk:
         :return:
         """
         random_walks = []
-        for _ in range(self.walk_per_node):
+        for _ in tqdm(range(self.walk_per_node)):
             random_nodes = list(g.nodes)
             random.shuffle(random_nodes)
-            for node in tqdm(random_nodes):
+            for node in random_nodes:
                 random_walks.append(self.random_walk(g=g, start=node, use_probabilities=use_probabilities))
         return random_walks
 
@@ -69,14 +69,84 @@ class DeepWalk:
 
 #----------------------------------------------------------------------------
 
+#start = ''
+#walk = [start]
+use_probabilities = False
+window_size = 5
+embedding_size = 100
+walk_length = 100
+walks_per_node = 20
+
+G = nx.karate_club_graph()
+
+
+
+start = random.choice(list(G.nodes()))
+start
+
+walk = [start]
+
+G.nodes(data=True)
+
+g = G
+i = 0
+
+g.get_edge_data(26,33)
+
+walk[i]
+neighbours = g.neighbors(walk[i])
+neighbours
+
+neighs = list(neighbours)
+neighs
+
+g[26]
+g.adj[26]
+
+
+for i in range(walk_length):
+    neighbours = g.neighbors(walk[i])
+    neighs = list(neighbours)
+    if use_probabilities:
+        probabilities = [g.get_edge_data(walk[i], neig)["weight"] for neig in neighs]
+        sum_probabilities = sum(probabilities)
+        probabilities = list(map(lambda t: t / sum_probabilities, probabilities))
+        p = np.random.choice(neighs, p=probabilities)
+    else:
+        p = random.choice(neighs)
+    walk.append(p)
+
+
+G = nx.path_graph(4)  # or DiGraph, MultiGraph, MultiDiGraph, etc
+
+G.nodes()
+
+[n for n in G.neighbors(0)]
+
+
+#--------------------------------------------
 import networkx as nx
 
 # load graph from networkx library
 G = nx.karate_club_graph()
 
-dw = DeepWalk(window_size = 5, embedding_size = 100, walk_length = 100, walks_per_node = 20)
+G.edges()
 
-my_rws = dw.get_walks(g = G, use_probabilities = True)
+dw = DeepWalk(window_size = 5, embedding_size = 100, walk_length = 10, walks_per_node = 2)
+
+for _ in tqdm(range(dw.walk_per_node)):
+    random_nodes = list(g.nodes)
+    random.shuffle(random_nodes)
+    random_walks = [dw.random_walk(g=g, start=node, use_probabilities=use_probabilities) for node in random_nodes]
+
+random_walks
+len(random_walks)
+
+len(random_nodes)
+
+dw.random_walk(g = G, use_probabilities = False)
+
+my_rws = dw.get_walks(g = G, use_probabilities = False)
 
 embed = dw.compute_embeddings(walks = my_rws)
 
